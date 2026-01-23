2ba72b8e5a37f9d301d51a5dcc54ab2eae56fade
idxinf <imfine233@outlook.com>
2026-01-23 10:25:40 +0800

    添加index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name            Remove Douyu Danmaku SFX - 屏蔽斗鱼高级弹幕特效
+// @namespace       https://github.com/idxinf/Remove-Douyu-Danmaku-SFX
+// @version         1.0.0
+// @description     屏蔽斗鱼高级弹幕特效
+// @author          idxinf
+// @license         MIT
+// @match			*://*.douyu.com/0*
+// @match			*://*.douyu.com/1*
+// @match			*://*.douyu.com/2*
+// @match			*://*.douyu.com/3*
+// @match			*://*.douyu.com/4*
+// @match			*://*.douyu.com/5*
+// @match			*://*.douyu.com/6*
+// @match			*://*.douyu.com/7*
+// @match			*://*.douyu.com/8*
+// @match			*://*.douyu.com/9*
+// @icon            https://www.douyu.com/favicon.ico
+// @grant           GM_addStyle
+// @downloadURL     
+// @updateURL       
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle(`css`);
+})();
